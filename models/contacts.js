640f603abb07cc0940f59b59aedadc3fcab212c9
Arkadiusz Daugielewicz <akson_control@o2.pl>
2024-02-15 04:28:34 +0100

    fix: listContacts()

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,36 @@
-// const fs = require('fs/promises')
+const fs = require("node:fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+// const baseDir = path.dirname("./");
+// const outFileName = path.basename("/models/contacts.json");
+// const contactsPath = path.join(baseDir, outFileName);
+const contactsPath = './models/contacts.json';
 
-const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    // console.log("contactsPath: ", contactsPath);
+    const data = await fs.readFile(contactsPath, "utf8");
+    const parseData = await JSON.parse(data);
+    return parseData;
+    // console.table(parseData);
+  } catch (error) {
+    console.log(error);
+  }
 
-const addContact = async (body) => {}
+  // const test = {
+  //   text: "func: listContacts"
+  // }
+  // return test;
+};
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {};
+
+const removeContact = async (contactId) => {};
+
+const addContact = async (body) => {};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +38,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
